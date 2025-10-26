cffacc75ce271d996a68c03dcc982a73cd6dec00
Ti live <timyr1995@bk.ru>
2025-10-26 19:59:51 +0500

    Added new settings for PC

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionPC"));
 });
 var app = builder.Build();
 
