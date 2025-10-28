bad7a6557df018a62c1cf28864a0702c3a9b92ce
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2025-10-28 19:06:55 +0500

    Added Categorise Controller

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+using LogProject.Data;
+using LogProject.Models.Domain;
+using LogProject.Models.DTO;
+using LogProject.Repositories.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            await  _categoryRepository.CreateAsync(category);
+         
+            var response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+        }
+    }
+}
